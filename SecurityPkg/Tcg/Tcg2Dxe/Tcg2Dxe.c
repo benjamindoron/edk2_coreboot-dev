f7cc2620e86b2da8a2d8b6d45083386a345cc5fe
Benjamin Doron <benjamin.doron00@gmail.com>
2020-11-20 23:18:07 -0500

    [PATCH] SecurityPkg/Tcg2Dxe: Apply fixes
    Apply fixes for TCBZ2590 and TCBZ2753. Cherry-picked https://github.com/microsoft/mu_basecore/commit/47ec8ed487afa0087c15818babb288ad0f0dd01f and https://github.com/microsoft/mu_basecore/commit/d423c1b4c46956f76c01f2de72ed60609d874ea1.
    
    Signed-off-by: Benjamin Doron <benjamin.doron00@gmail.com>

--- SecurityPkg/Tcg/Tcg2Dxe/Tcg2Dxe.c
+++ SecurityPkg/Tcg/Tcg2Dxe/Tcg2Dxe.c
@@ -2516,6 +2516,17 @@ OnExitBootServicesFailed (
   EFI_STATUS    Status;
 
   //
+  // Measure invocation of ExitBootServices,
+  //
+  Status = TcgMeasureAction (
+             5,
+             EFI_EXIT_BOOT_SERVICES_INVOCATION
+             );
+  if (EFI_ERROR (Status)) {
+    DEBUG ((EFI_D_ERROR, "%a not Measured. Error!\n", EFI_EXIT_BOOT_SERVICES_INVOCATION));
+  }
+
+  //
   // Measure Failure of ExitBootServices,
   //
   Status = TcgMeasureAction (
@@ -2759,7 +2770,7 @@ DriverEntry (
 
     Status = gBS->CreateEventEx (
                     EVT_NOTIFY_SIGNAL,
-                    TPL_NOTIFY,
+                    TPL_CALLBACK,
                     OnExitBootServices,
                     NULL,
                     &gEfiEventExitBootServicesGuid,
@@ -2771,7 +2782,7 @@ DriverEntry (
     //
     Status = gBS->CreateEventEx (
                     EVT_NOTIFY_SIGNAL,
-                    TPL_NOTIFY,
+                    TPL_CALLBACK,
                     OnExitBootServicesFailed,
                     NULL,
                     &gEventExitBootServicesFailedGuid,
